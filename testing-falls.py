f011d8750fdb96a5aca382e162419535c74b91cd
zgiannuzzi <zachgiannuzzi@yahoo.com>
2024-10-08 13:09:25 +0000

    reading in base file for participation

--- testing-falls.py
+++ testing-falls.py
@@ -8,7 +8,7 @@ root_data_directory = 'trouble_shooting/summer2024'
 #path_participation = f'{root_data_directory}/participation'
 #path_registration = f'{root_data_directory}/registration'
  
-propper_formated_df = pd.read_csv('trouble_shooting/summer2024/Participation_summer.csv', header =2)
+propper_formated_df = pd.read_csv('trouble_shooting/summer2024/Participation_summer.csv', header = 0,sep=',')
 propper_formated_df.head()
 
 propper_formated_df.to_excel(f'{root_data_directory}/Master_Participation_Data_Set.xlsx', index=False)
